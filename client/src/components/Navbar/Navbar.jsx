9c2a98bfe0fec79fe9e644a6a1e0d98f85bd14bd
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-07-01 21:26:29 -0500

    add images

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { images } from '../../constants'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src={images.logo} alt="" />
+        </div>
+        <ul>
+            {['home','about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+                <li key={`link-${item}`}>
+                    <div />
+                    <a href={`#{item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
