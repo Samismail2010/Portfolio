a51917fa51e8bad36b51a9f5a180f9b5d51ebcdd
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-07-13 20:36:02 -0500

    project section

--- client/src/container/Work/Work.jsx
+++ client/src/container/Work/Work.jsx
@@ -12,8 +12,19 @@ const Work = () => {
   const [filterWork, setFilterWork] = useState([]);
 
   const handleWorkFilter = (item) => {
-    
-  }
+      setActiveFilter(item);
+      setAnimateCard([{ y:100, opacity: 0}]);
+      
+      setTimeout(() => {
+        setAnimateCard([{ y:0, opacity: 1}]);
+
+        if(item ==='All') {
+          setFilterWork(works);
+        } else {
+          setFilterWork(works.filter((work) => work.tags.includes(item)));
+        }
+      }, 500)
+  };
 
   useEffect(() => {
     const query ='*[_type == "works"]';
@@ -21,8 +32,8 @@ const Work = () => {
     client.fetch(query).then((data) => {
     setWorks(data);
     setFilterWork(data);
-  })
-  }, [])
+  });
+  }, []); 
 
   return (
     <>
@@ -48,41 +59,41 @@ const Work = () => {
     >
       {filterWork.map((work, index) => (
         <div className='app__work-item app__flex' key={index}>
-          <div className='app__work-img app__flex'>
-            <img src={urlFor(work.imgUrl)}/>
+          <div 
+              className='app__work-img app__flex'
+              >
+            <img src={urlFor(work.imgUrl)} alt={work.name}/>
 
             <motion.div
-            whileHover={{opacity: [0, 1]}}
-            transition={{ duration: 0.25, ease: 'easeInOut ', staggerChildren: 0.5}}
-            className='app__work-hover app__flex'
-            >
-              {/* wraps cards in portfolio section */}
-              <a href={work.projectLink} target="_blank" rel='noreferrer'>
-                <motion.div
-                whileInView={{scale: [0, 1]}}
-                whileHover={{opacity: [1, 0.9]}}
-                transition={{ duration: 0.25 }}
-                className='app__flex'
-                >
-                  <AiFillEye />
-                </motion.div>
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
 
-              </a>
-              <a href={work.codeLink} target="_blank" rel='noreferrer'>
-                <motion.div
-                whileInView={{scale: [0, 1]}}
-                whileHover={{opacity: [1, 0.9]}}
-                transition={{ duration: 0.25 }}
-                className='app__flex'
-                >
-                  <AiFillGithub />
-                </motion.div>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
 
-              </a>
-
-            </motion.div>
-          </div>
-
+            {/* title and description */}
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
               <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
@@ -96,7 +107,7 @@ const Work = () => {
       ))}
     </motion.div>
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(Work, 'work');
